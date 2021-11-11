0954f1dea256919d88742c9c2df7e9a430176c33
anluse <anders.lundstrom@gmail.com>
2021-11-11 10:24:44 +0100

    Update item-sheet.js

--- module/sheets/item-sheet.js
+++ module/sheets/item-sheet.js
@@ -1,5 +1,5 @@
 export default class ChockItemSheet extends ItemSheet {
     get template(){
-        return `systems/foundry-vtt-chock/templates/sheets/${this.item.data.type}-sheet.html`;
+        return `systems/foundry-vtt-chock/templates/sheets/item-sheet.html`;
     }
 }
