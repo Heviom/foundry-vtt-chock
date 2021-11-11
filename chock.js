2f2ffbfc08004503f3c12279de7096e9f47df107
anluse <anders.lundstrom@gmail.com>
2021-11-11 10:29:56 +0100

    Update chock.js

--- chock.js
+++ chock.js
@@ -3,5 +3,5 @@ import ChockItemSheet from "./module/sheets/item-sheet.js"
 Hooks.once("init",function () {
     console.log("Chock | Initializing Chock System");
     Items.unregsiterSheet("core", ItemSheet);
-    Items.registerSheet("chock", ChockItemSheet, { makeDefault: true });
+    Items.registerSheet("foundry-vtt-chock", ChockItemSheet, { makeDefault: true });
 });
